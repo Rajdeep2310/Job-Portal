d092a9916ea7a55757ef8c9d4073b980f38c1a8c
Rajdeep2310 <rajdeepgkp23@gmail.com>
2024-04-07 14:52:21 +0530

    first commit

---
+++ Server/Server.js
@@ -0,0 +1,24 @@
+//Importing packages and functions...
+const express = require("express");
+const colors = require("colors")
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Some Middlewares...
+dotenv.config();
+app.use(express.json()); 
+
+// Database Connection ....
+mongoose.connect(process.env.MONGO_URL).then(() =>{
+    console.log(`Connected to MongoDB Database     `.italic.bgGreen);
+}).catch((err) =>{
+    console.log(err)
+});
+
+//Listening Port...
+app.listen(1000,() => {
+    console.log(`Server is running on port : 1000  `.italic.bgBlue)
+})
+
+// Application Routes ...
