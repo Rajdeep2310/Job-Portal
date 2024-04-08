6670eb57f3ecb08c2ec4e3b25dc0af0301306e34
Rajdeep2310 <rajdeepgkp23@gmail.com>
2024-04-09 02:11:05 +0530

    adding tailwind css and creating login and register page ui

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,20 @@
-import {BrowserRouter , Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Components/Homepage";
+import Loginpage from "./Components/Loginpage";
+import Registerpage from "./Components/Registerpage";
+import Notfound from "./Components/Notfound";
 
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/register" element={<Registerpage />} />
+        <Route path="/*" element={<Notfound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
-
+  );
+};
 
 export default App;
--- Client/src/Components/Homepage.jsx
+++ Client/src/Components/Homepage.jsx
@@ -1,7 +1,6 @@
-// import styles from "./styles/Homepage.module.css";
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <div className="text-3xl font-bold underline">Homepage</div>
   )
 }
